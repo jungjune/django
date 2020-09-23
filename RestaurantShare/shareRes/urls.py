1f5b71132d7b341d8e2603df44b99e4030ba3d0a
이정준 <jungjun1271@gmail.com>
2020-09-23 12:02:03 +0000

    맛집 공유  첫번째

---
+++ RestaurantShare/shareRes/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('restaurantDetail/', views.restaurantDetail, name='restaurantDetail'),
+    path('restaurantCreate/', views.restaurantCreate, name='restaurantCreate'),
+    path('categoryCreate/', views.categoryCreate, name='categoryCreate'),
+]
