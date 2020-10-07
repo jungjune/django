e14ab72de13ee65769fb6afc48082988eae56530
이정준 <jungjun1271@gmail.com>
2020-10-07 12:52:31 +0000

    20201007 create and delete

--- RestaurantShare/shareRes/urls.py
+++ RestaurantShare/shareRes/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('restaurantDetail/', views.restaurantDetail, name='restaurantDetail'),
     path('restaurantCreate/', views.restaurantCreate, name='restaurantCreate'),
-    path('categoryCreate/', views.categoryCreate, name='categoryCreate'),
+    path('categoryCreate/', views.categoryCreate, name='cateCreatePage'),
+    path('categoryCreate/create', views.Create_category, name='cateCreate'),
+    path('categoryCreate/delete', views.Delete_category, name='cateDelete'),
 ]
